b2042a600eb77b283d47a24fc5c2d67f39e4ffa3
Louis Mille <mille.louis1@gmail.com>
2023-08-21 19:23:01 +0200

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Louis Mille on 21/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
